46e30b40e521c8720977f17b90ffb2e1157d7ffd
zz920 <zhibowen@126.com>
2019-03-14 19:37:29 +0000

    remove the unique constraint of category

--- dashboard/souq/models/category.py
+++ dashboard/souq/models/category.py
@@ -5,4 +5,4 @@ class Category(models.Model):
 
     name = models.CharField(max_length=50, unique=True)
     classification = models.CharField(max_length=50)
-    link = models.CharField(max_length=250, unique=True, db_index=True)
+    link = models.CharField(max_length=250, db_index=True)
