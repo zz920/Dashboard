c78d52f2fcfeedc8dfee362a94c35ec8ab91865f
Bowen zhi <b.zhi@fetchr.us>
2019-04-06 07:43:23 -0700

    modify init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y python3-pip libmysqlclient-dev nginx virtualenv
+sudo virtualenv --python=python3 /opt/env
+source /opt/env/bin/active
 sudo pip install -r requirements.txt
+cp ./dashboard.conf /etc/supervisor/conf.d/dashboard.conf
+sudo supervisordctl update
+sudo ln -s /opt/Dashboard/nginx.conf /etc/nginx/site-enable/dashboard.org
