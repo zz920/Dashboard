b74c977a20195e1cc7e7a6ec6954974d021a0764
Bowen Zhi <b.zhi@fetchr.us>
2019-04-30 17:20:52 +0400

    add proxy model in router

--- dashboard/dbrouter.py
+++ dashboard/dbrouter.py
@@ -1,21 +1,22 @@
 from souq.mongo_models import MCategory, MItem, MSeller
-from souq.models import Category, Item, Seller, Detail
+from souq.models import *
 
 
 class CustomDBRouter(object):
     SOUQ_MONGO_MODEL = (MCategory, MItem, MSeller)
     SOUQ_PSQL_MODEL = (Category, Item, Seller, Detail)
+    SOUQ_PSQL_PROXY_MODEL = (HotItem, SingleItem, SingleCategory, SingleSeller)
 
     def db_for_read(self, model, **hints):
         if model in self.SOUQ_MONGO_MODEL:
             return 'mongo_souq_ksa'
-        if model in self.SOUQ_PSQL_MODEL:
+        if model in self.SOUQ_PSQL_MODEL or model in self.SOUQ_PSQL_PROXY_MODEL:
             return 'psql_souq_ksa'
         return None
 
     def db_for_write(self, model, **hints):
         if model in self.SOUQ_MONGO_MODEL:
             return 'mongo_souq_ksa'
-        if model in self.SOUQ_PSQL_MODEL:
+        if model in self.SOUQ_PSQL_MODEL or model in self.SOUQ_PSQL_PROXY_MODEL:
             return 'psql_souq_ksa'
         return None
